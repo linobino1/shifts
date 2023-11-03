d2c7d0826d95b9df089bb647213b675baa1e6258
Leo <leo.hilsheimer@gmail.com>
2023-11-03 10:49:53 +0100

    initial payload setup

--- payload.config.ts
+++ payload.config.ts
@@ -1,11 +1,9 @@
-import type { RichTextAdapter } from "payload/types";
 import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { lexicalEditor } from "@payloadcms/richtext-lexical";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
 import { buildConfig } from "payload/config";
 import path from "path";
 import Users from "./cms/collections/Users";
-import Media from "./cms/collections/Media";
 
 export default buildConfig({
   serverURL: process.env.PAYLOAD_PUBLIC_SERVER_URL || "http://localhost:3000",
@@ -13,7 +11,7 @@ export default buildConfig({
     user: Users.slug,
     bundler: webpackBundler(),
   },
-  editor: lexicalEditor({}) as RichTextAdapter,
+  editor: lexicalEditor({}),
   db: mongooseAdapter({
     url: process.env.MONGO_URL ?? false,
     connectOptions: {
@@ -22,7 +20,6 @@ export default buildConfig({
   }),
   collections: [
     Users,
-    Media,
   ],
   typescript: {
     outputFile: path.resolve(__dirname, "cms/payload-types.ts"),
